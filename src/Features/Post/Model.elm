e8f85ad3e976b0c66542f1a21eef4bd62643674c
balovbohdan <balovbohdan@gmail.com>
2021-11-07 21:42:38 +0200

    styles

--- src/Features/Post/Model.elm
+++ src/Features/Post/Model.elm
@@ -1,12 +1,19 @@
-module Features.Post.Model exposing (queryPostFeatureContent, parsePostFeatureContent)
+module Features.Post.Model exposing (decodePostMeta, queryPostFeatureContent, parsePostFeatureContent)
 
 import Http
 import Base64
 import Json.Decode
 
 import Core.Message exposing (Message(..))
+import Array
 
 type alias PostFeatureContent = { name: String, content: String }
+
+type alias PostMeta =
+  { title: String
+  , description: String
+  , keywords: List String
+  }
 
 queryPostFeatureContent : String -> Cmd Message
 queryPostFeatureContent id =
@@ -21,10 +28,26 @@ postFeatureContentDecoder =
     (Json.Decode.field "name" Json.Decode.string)
     (Json.Decode.field "content" Json.Decode.string)
 
+decodePostMetaItem : Maybe String -> String
+decodePostMetaItem metaItem =
+  case (metaItem) of
+    Nothing -> ""
+    Just value -> value
+
+decodePostMeta : String -> PostMeta
+decodePostMeta text =
+  let
+    metaItems = Array.fromList (List.take 8 (String.lines text))
+  in
+    { title = decodePostMetaItem (Array.get 1 metaItems)
+    , description = decodePostMetaItem (Array.get 2 metaItems)
+    , keywords = String.split "," (decodePostMetaItem (Array.get 3 metaItems))
+    }
+
 decodePostText : String -> String
 decodePostText text =
   let
-    formattedText = String.replace "\n" "" text
+    formattedText = String.concat (List.drop 7 (String.lines text))
   in
     case (Base64.decode formattedText) of
       Ok result -> result
