2ebb0c7507e21719a35c6ae6b6e26037b15230e6
Bohdan Balov <balovbohdan@gmail.com>
2021-11-24 22:37:49 +0200

    Navigation Improvements (#6)
    * style improvements
    
    * gitignore
    
    * medical app idea
    
    * index
    
    * 404
    
    * hash routing
    
    * 404
    
    * manifest, favicons
    
    * index.js
    
    * load more (in progress)
    
    * post -> posts
    
    * removed index.js
    
    * header
    
    * page title
    
    * removed index

--- src/Core/Layout.elm
+++ src/Core/Layout.elm
@@ -2,8 +2,8 @@ module Core.Layout exposing (layout)
 
 import Css
 import Css.Global
-import Html.Styled exposing (div, Html)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled exposing (a, div, img, span, text, Html)
+import Html.Styled.Attributes exposing (css, src, href, title)
 
 import Core.Model.Types exposing (Model)
 import Core.Message exposing (Message)
@@ -16,6 +16,43 @@ globalCss model =
   Css.Global.global
     [ Css.Global.selector "body" [ Css.backgroundColor model.theme.background ]
     , Css.Global.selector "p, a, ol, ul, li, span, h1, h2, h3, h4, h5, h6" [ Css.color model.theme.textPrimary ]
+    ]
+
+socialButton : String -> String -> String -> Html Message
+socialButton name socialHref imageUrl =
+  a
+    [ title name, href socialHref ]
+    [ img
+        [ src imageUrl
+        , css [ Css.width <| Css.px 30, Css.marginLeft <| Css.px 10 ]
+        ]
+        []
+    ]
+
+footer : Model -> Html Message
+footer model =
+  div
+    [ css
+        [ Css.displayFlex
+        , Css.alignItems Css.center
+        , Css.justifyContent Css.spaceBetween
+        , Css.height <| Css.px 100
+        , Css.marginTop <| Css.px 100
+        , Css.borderTop3 (Css.px 1) Css.solid model.theme.primaryLight
+        ]
+    ]
+    [ span [ css [ Css.color model.theme.primaryLight ] ] [ text "All rights reserved" ]
+    , div
+        [ css [ Css.displayFlex ] ]
+        [ socialButton
+            "GitHub"
+            "https://github.com/balovbohdan"
+            "https://raw.githubusercontent.com/balovbohdan/balovbohdan.github.io/main/docs/assets/github.svg"
+        , socialButton
+            "Twitter"
+            "https://twitter.com/balov_bohdan"
+            "https://raw.githubusercontent.com/balovbohdan/balovbohdan.github.io/main/docs/assets/twitter.svg"
+        ]
     ]
 
 layout : Props -> Html Message
@@ -34,4 +71,5 @@ layout props =
         , css = [ Css.marginTop <| Css.px 50 ]
         }
     , div [ css [ Css.marginTop <| Css.px 50 ] ] [ props.feature ]
+    , footer props.model
     ]
--- src/Core/Utils/PageTitle.elm
+++ src/Core/Utils/PageTitle.elm
@@ -1,10 +1,27 @@
 module Core.Utils.PageTitle exposing (getPageTitle)
 
+import Array
+
 import Core.Model.Types exposing (Model)
+import Model.PostMeta.Decoder exposing (decodePostMeta)
+
+defaultTitle : String
+defaultTitle = "Mr. Balov | Personal Blog"
 
 getPageTitle : Model -> String
 getPageTitle model =
-  case model.url.path of
-    "/" -> "Bohdan Balov | Personal Blog"
-    "/post" -> "Awesome Post | Bohdan Balov"
-    _ -> "Bohdan Balov | Personal Blog"
+  let
+    pathPartsRaw = String.split "/" model.url.path
+    pathParts = List.filter (\pathPart -> pathPart /= "") pathPartsRaw
+    firstPathPart = Maybe.withDefault "" (List.head pathParts)
+  in
+    case (firstPathPart) of
+      "posts" ->
+        let
+          postMetaRaw = Array.get 1 model.featureData.content
+          postMetaDecoded = decodePostMeta postMetaRaw
+          postTitle = if postMetaDecoded.title == "" then "Awesome Article..." else postMetaDecoded.title
+        in
+          postTitle ++ " | Mr. Balov"
+      "" -> defaultTitle
+      _ -> defaultTitle
