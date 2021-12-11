22f9a772572fe20d3e879148722ac1a00f92cb23
Bohdan Balov <balovbohdan@gmail.com>
2021-12-11 21:54:39 +0200

    Docs -> Dist (#20)
    * style improvements
    
    * gitignore
    
    * medical app idea
    
    * index
    
    * 404
    
    * hash routing
    
    * 404
    
    * manifest, favicons
    
    * index.js
    
    * load more (in progress)
    
    * post -> posts
    
    * removed index.js
    
    * header
    
    * page title
    
    * removed index
    
    * home section - about author
    
    * removed index.js
    
    * self review fixes
    
    * removed index.js
    
    * updated background color
    
    * removed index.js
    
    * removed index.js
    
    * Delete author-1.jpeg
    
    * author
    
    * in progress
    
    * author page
    
    * removed index.js
    
    * removed redundant file
    
    * removed index.js
    
    * code parser middleware
    
    * in progress
    
    * webpack
    
    * webpack
    
    * elmroot changed
    
    * removed dplicated files
    
    * fixes
    
    * self review fixes
    
    * webpack config
    
    * fix
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * workflow
    
    * gitignore
    
    * reverted code-block
    
    * Update publish.yml
    
    * Update .gitignore
    
    * removed docs
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update package.json
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update .gitignore
    
    * Update webpack.config.js
    
    * Update Author.elm
    
    * Update publish.yml
    
    * fix
    
    * webpack
    
    * webpack
    
    * docs -> dist
    
    * self review fixes

--- src/elm/Model/Post/Config.elm
+++ src/elm/Model/Post/Config.elm
@@ -4,6 +4,6 @@ type alias Config = { url: String, extension: String }
 
 config : Config
 config =
-  { url = "https://api.github.com/repos/balovbohdan/mr-balov-blog/contents/docs/content/blog/posts/"
+  { url = "https://api.github.com/repos/balovbohdan/mr-balov-blog/contents/dist/content/blog/posts/"
   , extension = ".md"
   }
