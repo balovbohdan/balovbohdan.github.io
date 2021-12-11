438c2f54deaa7d426a15f506f52eac07d0056b25
balovbohdn <balovbohdan@gmail.com>
2021-12-11 22:03:58 +0200

    dist -> nothing

--- src/elm/Model/Post/Config.elm
+++ src/elm/Model/Post/Config.elm
@@ -4,6 +4,6 @@ type alias Config = { url: String, extension: String }
 
 config : Config
 config =
-  { url = "https://api.github.com/repos/balovbohdan/mr-balov-blog/contents/dist/content/blog/posts/"
+  { url = "https://api.github.com/repos/balovbohdan/mr-balov-blog/contents/content/blog/posts/"
   , extension = ".md"
   }
