8a25024a33e6bb1977bedfad3990131dc0f48557
balovbohdn <balovbohdan@gmail.com>
2022-01-10 09:47:17 +0200

    artile react ioc meta

---
+++ src/content/blog/metas/what-you-need-to-know-about-the-ioc.elm
@@ -0,0 +1,6 @@
+| Key           | Value                                                                                                                                             |
+| ------------- | ------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Title         | What you need to know about the inversion of control                                                                                              |
+| Description   | Inversion of Control principle applied to React projects                                                                                          |
+| Keywords      | javascript, frontend, best practicies, react, inversion of control                                                                                |
+| Cover         | https://raw.githubusercontent.com/balovbohdan/balovbohdan.github.io/github-page/content/blog/posts/what-you-need-to-know-about-the-ioc/cover.jpg  |
