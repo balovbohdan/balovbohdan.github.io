6a074e4d359d0034d930c716ea0c8effdecd2c1d
balovbohdn <balovbohdan@gmail.com>
2022-01-12 22:01:31 +0200

    meta

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import 'codemirror-elm';
+import { Base64 } from 'js-base64';
 
 import { Elm } from './elm/Main.elm';
 
@@ -19,12 +20,45 @@ const getColorSchema = () => {
   return colorSchema ? parseJson(defaultColorSchema, colorSchema) : defaultColorSchema;
 };
 
+const decodePostMeta = (data) => {
+  const postMetaTable = Base64.decode(data.content);
+  const postMetaTableLines = postMetaTable.split('\n').slice(2);
+  const postMeta = postMetaTableLines.reduce((result, line) => {
+    if (line) {
+      const [key, value] = line.split('|').map((item) => item.trim()).filter(Boolean);
+
+      return { ...result, [key.toLocaleLowerCase()]: value };
+    } else {
+      return result;
+    }
+  }, {});
+
+  return postMeta;
+};
+
+const setMeta = (meta) => {
+  const head = document.querySelector('head');
+
+  head.querySelector('title').innerText = meta.title;
+  head.querySelector('meta[name="description"]').setAttribute('content', meta.description);
+  head.querySelector('meta[name="keywords"]').setAttribute('content', meta.keywords);
+
+  head.querySelector('meta[property="og:title"]').setAttribute('content', meta.title);
+  head.querySelector('meta[property="og:description"]').setAttribute('content', meta.description);
+  head.querySelector('meta[property="og:image"]').setAttribute('content', meta.image);
+  head.querySelector('meta[property="og:url"]').setAttribute('content', window.location.href);
+
+  head.querySelector('meta[property="twitter:title"]').setAttribute('content', meta.title);
+  head.querySelector('meta[property="twitter:description"]').setAttribute('content', meta.description);
+  head.querySelector('meta[name="twitter:image:src"]').setAttribute('content', meta.image);
+};
+
 const app = Elm.Main.init({
   node: document.getElementById('app'),
   flags: { colorSchema: getColorSchema() },
 });
 
-app.ports.localStorageOutcomePort.subscribe((event) => {
+const handleLocalStorageOutcomePort = (event) => {
   const { action, key, value } = parseJson({}, event);
 
   switch (action) {
@@ -37,4 +71,29 @@ app.ports.localStorageOutcomePort.subscribe((event) => {
     default:
       console.warn('Cannot find localStorage action: ' + action);
   }
-});
+};
+
+const handleMetaChangePort = (event) => {
+  const isPostPage = /\/posts\/\w+/.test(window.location.href);
+
+  if (isPostPage) {
+    const postMeta = decodePostMeta(JSON.parse(event[1]));
+
+    setMeta({
+      title: postMeta.title,
+      description: `${postMeta.description} | Mr. Balov`,
+      keywords: postMeta.keywords,
+      image: postMeta.cover,
+    });
+  } else {
+    setMeta({
+      title: 'Mr. Balov | Personal Blog',
+      description: 'Software Development Blog by Mr. Balov',
+      keywords: 'javascript, frontend, software development, react',
+      image: 'https://balovbohdan.github.io/assets/author-2.jpg',
+    });
+  }
+};
+
+app.ports.localStorageOutcomePort.subscribe(handleLocalStorageOutcomePort);
+app.ports.metaChangePort.subscribe(handleMetaChangePort);
