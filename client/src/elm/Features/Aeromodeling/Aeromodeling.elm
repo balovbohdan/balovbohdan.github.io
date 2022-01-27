af25c781e4be61a767ef6e5be0680eb229e542f1
balovbohdn <balovbohdan@gmail.com>
2022-01-27 23:22:31 +0200

    removed broken aeromodeling video

--- client/src/elm/Features/Aeromodeling/Aeromodeling.elm
+++ client/src/elm/Features/Aeromodeling/Aeromodeling.elm
@@ -59,16 +59,17 @@ freeFlightVideo =
             [ Css.flexBasis <| Css.pct 100, Css.height <| Css.vh 70 ]
         ]
     ]
-    [ iframe
-        [ src "https://www.youtube.com/embed/AAHrMaOCd1c"
-        , property "allow" (Json.Encode.string "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture")
-        , css
-            [ Css.width <| Css.pct 100
-            , Css.height <| Css.pct 100
-            , Css.maxHeight <| Css.px 400
-            ]
-        ]
-        []
+    [
+      -- iframe
+      --   [ src "https://www.youtube.com/embed/AAHrMaOCd1c"
+      --   , property "allow" (Json.Encode.string "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture")
+      --   , css
+      --       [ Css.width <| Css.pct 100
+      --       , Css.height <| Css.pct 100
+      --       , Css.maxHeight <| Css.px 400
+      --       ]
+      --   ]
+      --   []
     ]
 
 freeFlightVideoDescription : Html Message
