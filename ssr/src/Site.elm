96d20cd9e3c55bf5cbbcd0c8b5df530f32c77e92
balovbohdn <balovbohdan@gmail.com>
2022-02-13 14:21:38 +0200

    in progress

--- ssr/src/Site.elm
+++ ssr/src/Site.elm
@@ -1,41 +1,32 @@
 module Site exposing (config)
 
-import DataSource
 import Head
-import Pages.Manifest as Manifest
 import Route
-import SiteConfig exposing (SiteConfig)
+import DataSource
+import Pages.Manifest
+import SiteConfig
 
+type alias Data = ()
 
-type alias Data =
-    ()
-
-
-config : SiteConfig Data
+config : SiteConfig.SiteConfig Data
 config =
-    { data = data
-    , canonicalUrl = "https://elm-pages.com"
-    , manifest = manifest
-    , head = head
-    }
-
+  { data = data
+  , manifest = manifest
+  , head = head
+  , canonicalUrl = "https://balovbohdan.github.io/"
+  }
 
 data : DataSource.DataSource Data
-data =
-    DataSource.succeed ()
-
+data = DataSource.succeed ()
 
 head : Data -> List Head.Tag
-head static =
-    [ Head.sitemapLink "/sitemap.xml"
-    ]
+head static = [ Head.sitemapLink "/sitemap.xml" ]
 
-
-manifest : Data -> Manifest.Config
+manifest : Data -> Pages.Manifest.Config
 manifest static =
-    Manifest.init
-        { name = "Site Name"
-        , description = "Description"
-        , startUrl = Route.Index |> Route.toPath
-        , icons = []
-        }
+  Pages.Manifest.init
+    { name = "Mr. Balov | Software Development Blog"
+    , description = "Software Development Blog by Mr. Balov"
+    , startUrl = Route.Index |> Route.toPath
+    , icons = []
+    }
